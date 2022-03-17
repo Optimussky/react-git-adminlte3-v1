d8c763900e83f02f62710130d67c179904954a8e
optimussky05 <optimussky05@gmail.com>
2022-03-17 05:21:57 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Aside from "./components/Aside";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Preloader from "./components/Preloader";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+     <Preloader/> 
+      <Header/>
+      <Aside/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Preloader() {
+  return (
+    
+    
+    <div className="preloader flex-column justify-content-center align-items-center">
+      <img className="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60"/>
+    </div>
+  )
+}
